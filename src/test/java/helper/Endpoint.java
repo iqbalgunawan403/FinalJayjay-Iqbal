dd9d14869dc7402e7fda900f242ad64866cde7b8
iqbalgunawan403 <iqbalgunawan403@gmail.com>
2025-04-09 12:31:34 +0700

    first commit

---
+++ src/test/java/helper/Endpoint.java
@@ -0,0 +1,16 @@
+package helper;
+
+public class Endpoint {
+
+    public static final String host_dummy = "https://dummyapi.io/data/v1/";
+
+    public static final String GET_USER_BY_ID = host_dummy + "user/60d0fe4f5311236168a109cc";
+
+    public static final String CREATE_USER = host_dummy + "user/create";
+
+    public static final String DELETE_USER = host_dummy + "user";
+
+    public static final String UPDATE_USER = host_dummy + "user";
+
+
+}
